1eff01f624fdbe59ec635541b1ddee2b9059781a
NonokaIkenaga <chiyongnonoka2@gmail.com>
2024-08-22 21:30:53 +0900

    Calculation of the central coordinates

--- lib/stl_operator.f90
+++ lib/stl_operator.f90
@@ -23,6 +23,22 @@ module stl_operator_m
         double precision center(3), length(3), half_length(3)
     end type
 
+    type merge_triangle_t
+        type(coordinate_t) vertices(500) !決め打ち
+    end type
+
+    type re_merge_triangle_t
+        type(coordinate_t) vertices(500)
+    end type
+
+    type append_t
+        integer vertexID
+    end type
+
+    type re_append_t
+        integer vertexID
+    end type
+
     type box_t
         type(coordinate_t), allocatable :: node(:)
     end type
@@ -31,6 +47,10 @@ module stl_operator_m
         type(triangle_t),allocatable :: triangle(:)
         type(content_t), allocatable :: sorted_array(:)
         type(group_of_norm_t), allocatable :: group_norm(:)
+        type(merge_triangle_t), allocatable :: merge_triangles(:)
+        type(re_merge_triangle_t), allocatable :: re_merge_triangles(:)
+        type(append_t), allocatable :: append_IDs(:)
+        type(re_append_t), allocatable :: re_append_IDs(:)
         integer num_triangles, cnt_diff_norm
         double precision basic_vector(3)
         character(80) header
@@ -46,6 +66,10 @@ module stl_operator_m
         procedure get_min_max_coordinate_of_group
         procedure output_box_list
         procedure output_box_vtk
+        procedure output_coordinate_csv
+        procedure make_triangle_groups
+        procedure output_stl_triangle_group
+        procedure output_csv_triangle_group
 
     end type
 
@@ -130,12 +154,47 @@ module stl_operator_m
 
     end subroutine
 
+    subroutine output_coordinate_csv(self, filename)
+        class(stl_t) self
+        character(*), intent(in) :: filename
+        integer n_unit, faceID
+
+        open(newunit = n_unit, file = filename, status = "replace")
+            do faceID = 1, self%num_triangles
+                write(n_unit, '(*(g0:,"  "))') faceID
+                write(n_unit, '(*(g0:," , "))') self%triangle(faceID)%vertexID(1)%coordinate
+                write(n_unit, '(*(g0:," , "))') self%triangle(faceID)%vertexID(2)%coordinate
+                write(n_unit, '(*(g0:," , "))') self%triangle(faceID)%vertexID(3)%coordinate
+            end do
+        close(n_unit)
+
+    end subroutine
+
+    subroutine output_stl_triangle_group(self, faceID, filename)
+        class(stl_t), intent(in) :: self
+        integer n_unit, faceID, vertexID
+        character(*), intent(in) ::  filename
+
+        open(newunit = n_unit, file = "./data/triangle/stl/"//trim(filename), status = "replace")
+            write(n_unit,"(a)") "solid test"
+            do vertexID = 1, self%append_IDs(faceID)%vertexID - 1, 3
+            write(n_unit,'(4x,*(g0:," "))') "facet normal", self%triangle(faceID)%normal_vector
+            write(n_unit,"(8x,a)") "outer loop"
+            write(n_unit,'(12x,*(g0:," "))') "vertex", self%merge_triangles(faceID)%vertices(vertexID)%coordinate
+            write(n_unit,'(12x,*(g0:," "))') "vertex", self%merge_triangles(faceID)%vertices(vertexID + 1)%coordinate
+            write(n_unit,'(12x,*(g0:," "))') "vertex", self%merge_triangles(faceID)%vertices(vertexID + 2)%coordinate
+            write(n_unit,"(8x,a)") "endloop"
+            write(n_unit,"(4x,a)") "endfacet"
+            end do
+        close(n_unit)
+    end subroutine
+
     subroutine output_dot_with_basic_vector(self)
         class(stl_t) self
         integer n_unit, faceID
 
         ! 面数が合わない場合はここを変える.(任意の数)
-        self%basic_vector(:) = (/0.1,0.2,0.3/)
+        self%basic_vector(:) = (/0.1,0.1,0.1/)
 
         open(newunit = n_unit, file = "data/dot_with_basic_vector.txt", status = "replace")
             do faceID = 1, self%num_triangles
@@ -144,6 +203,228 @@ module stl_operator_m
             end do            
         close(n_unit)
 
+    end subroutine
+
+    subroutine make_triangle_groups(self)
+        implicit none
+        class(stl_t), intent(inout) :: self
+        integer faceID, vertexID, mergevertexID, n_unit, appendfaceID, new_size, &
+        appendvertexID_1, appendvertexID_2, i, mergevertexID_1, mergevertexID_2, mergefaceID, &
+        test_faceID, test_vertexID, re_appendvertexID, re_mergevertexID, del_vertexID, dummyID,&
+        update_appendID, update_vertexID, update_faceID, count, logical_test_ID
+        real index_value, coordinate(3), merge_coordinate(3), test_coordinate(3), &
+        re_appendcoordinate(3), replace_coordinate(3)
+        logical, allocatable :: classification_not_completed(:)
+        logical all_false
+        character(100) filename
+
+        allocate(self%merge_triangles(100)) !決め打ち．気管支末端部の面の数より多ければ良い
+        allocate(self%append_IDs(100)) !上の数字と同じにする
+
+        !初期配列値
+        do faceID = 1, size(self%merge_triangles)
+            do vertexID = 1, size(self%merge_triangles(faceID)%vertices)
+                self%merge_triangles(faceID)%vertices(vertexID)%coordinate = (/-99,-99,-99/) !ダミー値-99を代入
+                self%merge_triangles(1)%vertices(1)%coordinate = self%triangle(1)%vertexID(1)%coordinate
+                self%merge_triangles(1)%vertices(2)%coordinate = self%triangle(1)%vertexID(2)%coordinate
+                self%merge_triangles(1)%vertices(3)%coordinate = self%triangle(1)%vertexID(3)%coordinate
+            end do
+        end do
+
+        do i = 1, size(self%append_IDs)
+            self%append_IDs(i)%vertexID = 1
+        end do
+
+        appendfaceID = 2
+        do faceID =1, self%num_triangles
+            print*, "triangleID : ", faceID
+            mergeloop:do mergefaceID = 1, appendfaceID - 1
+                do mergevertexID = 1, size(self%merge_triangles(faceID)%vertices)
+                    do vertexID = 1, 3
+                        coordinate = self%triangle(faceID)%vertexID(vertexID)%coordinate
+                        merge_coordinate = self%merge_triangles(mergefaceID)%vertices(mergevertexID)%coordinate
+                        index_value = norm2(coordinate - merge_coordinate)
+                        ! print*, index_value
+                        if(index_value == 0) then !一致する座標があるとき
+                            ! print*, "same coordinate"
+                            ! print*, "mergefaceID = ", mergefaceID
+                            appendvertexID_1 = self%append_IDs(mergefaceID)%vertexID
+                            do mergevertexID_1 = 1, 3
+                                coordinate = self%triangle(faceID)%vertexID(mergevertexID_1)%coordinate
+                                self%merge_triangles(mergefaceID)%vertices(appendvertexID_1)%coordinate = coordinate
+                                self%append_IDs(mergefaceID)%vertexID = self%append_IDs(mergefaceID)%vertexID + 1
+                                appendvertexID_1 = self%append_IDs(mergefaceID)%vertexID
+                            end do
+                            exit mergeloop
+                        else if(vertexID == 3 .and. mergevertexID == size(self%merge_triangles(appendfaceID - 1)%vertices)&
+                                .and. mergefaceID == appendfaceID -1) then
+                            ! print*, "not same coordinate"
+                            ! print*, "mergefaceID = ", appendfaceID
+                            appendvertexID_2 = self%append_IDs(appendfaceID)%vertexID
+                            do mergevertexID_2 = 1, 3
+                                coordinate = self%triangle(faceID)%vertexID(mergevertexID_2)%coordinate
+                                self%merge_triangles(appendfaceID)%vertices(appendvertexID_2)%coordinate = coordinate
+                                self%append_IDs(appendfaceID)%vertexID = self%append_IDs(appendfaceID)%vertexID + 1
+                                appendvertexID_2 = self%append_IDs(appendfaceID)%vertexID
+                            end do
+                            appendfaceID = appendfaceID + 1
+                            exit mergeloop
+                        end if
+                    end do
+                end do
+            end do mergeloop
+        end do
+
+        !全て格納した後，かぶりがないか再度チェック
+        allocate(self%re_merge_triangles(100)) !merge_trianglesと同じ数
+        allocate(self%re_append_IDs(100)) !append_IDsと同じ数
+
+        do faceID = 1, size(self%merge_triangles)
+            do update_vertexID = 1, size(self%merge_triangles(faceID)%vertices)
+                self%re_merge_triangles(faceID)%vertices(update_vertexID)%coordinate&
+                = self%merge_triangles(faceID)%vertices(update_vertexID)%coordinate
+            end do
+        end do
+        do update_appendID = 1, size(self%append_IDs)
+            self%re_append_IDs(update_appendID)%vertexID = self%append_IDs(update_appendID)%vertexID
+        end do
+
+        allocate(classification_not_completed(size(self%merge_triangles)))
+        do faceID = 1, size(self%merge_triangles)
+            if(self%append_IDs(faceID)%vertexID - 1 == 0) then
+                classification_not_completed(faceID) = .false.
+            else
+                classification_not_completed(faceID) = .true.
+            end if
+        end do
+
+        all_false = all(.not. classification_not_completed)
+
+        do while(.not. all_false)
+            do faceID = 2, size(self%merge_triangles)
+                if(self%append_IDs(faceID)%vertexID - 1 > 0) then
+                    do test_faceID = 1, faceID - 1
+                        do test_vertexID = 1, self%append_IDs(test_faceID)%vertexID - 1
+                            do vertexID = 1, self%append_IDs(faceID)%vertexID - 1
+                                coordinate = self%merge_triangles(faceID)%vertices(vertexID)%coordinate
+                                test_coordinate = self%merge_triangles(test_faceID)%vertices(test_vertexID)%coordinate
+                                index_value = norm2(coordinate - test_coordinate)
+                                if(index_value == 0) then
+                                    !検出されたvertexIDが3で割れるかどうかの場合分け
+                                    if(mod(vertexID, 3) /= 0) then
+                                        re_appendvertexID = self%re_append_IDs(test_faceID)%vertexID
+                                        do re_mergevertexID = 3*int(vertexID/3)+1, 3*int(vertexID/3 + 1)
+                                            !一致groupにtriangleの座標を追加
+                                            re_appendcoordinate = self%merge_triangles(faceID)%vertices(re_mergevertexID)%coordinate
+                                            self%re_merge_triangles(test_faceID)%vertices(re_appendvertexID)%coordinate &
+                                            = re_appendcoordinate
+                                            self%re_append_IDs(test_faceID)%vertexID = self%re_append_IDs(test_faceID)%vertexID + 1
+                                            re_appendvertexID = self%re_append_IDs(test_faceID)%vertexID
+                                        end do
+                                        !元のgroupから追加したtriangleの座標を削除(置き換え)
+                                        do del_vertexID = 3*int(vertexID/3)+1, self%append_IDs(faceID)%vertexID - 4
+                                            replace_coordinate = self%merge_triangles(faceID)%vertices(del_vertexID + 3)%coordinate
+                                            self%re_merge_triangles(faceID)%vertices(del_vertexID)%coordinate = replace_coordinate
+                                        end do
+                                        !ダミー値を増やす&append_IDsの更新
+                                        do dummyID = self%append_IDs(faceID)%vertexID - 3, self%append_IDs(faceID)%vertexID - 1
+                                            self%re_merge_triangles(faceID)%vertices(dummyID)%coordinate = (/-99,-99,-99/)
+                                        end do
+                                        self%re_append_IDs(faceID)%vertexID = self%append_IDs(faceID)%vertexID - 3
+                                    else
+                                        re_appendvertexID = self%re_append_IDs(test_faceID)%vertexID
+                                        do re_mergevertexID = 3*int(vertexID/3)-2, 3*int(vertexID/3)
+                                            !一致groupにtriangleの座標を追加
+                                            re_appendcoordinate = self%merge_triangles(faceID)%vertices(re_mergevertexID)%coordinate
+                                            self%re_merge_triangles(test_faceID)%vertices(re_appendvertexID)%coordinate &
+                                            = re_appendcoordinate
+                                            self%re_append_IDs(test_faceID)%vertexID = self%re_append_IDs(test_faceID)%vertexID + 1
+                                            re_appendvertexID = self%re_append_IDs(test_faceID)%vertexID
+                                        end do
+                                        !元のgroupから追加したtriangleの座標を削除
+                                        do del_vertexID = 3*int(vertexID/3)-2, self%append_IDs(faceID)%vertexID - 4
+                                            replace_coordinate = self%merge_triangles(faceID)%vertices(del_vertexID + 3)%coordinate
+                                            self%re_merge_triangles(faceID)%vertices(del_vertexID)%coordinate = replace_coordinate
+                                        end do
+                                        !ダミー値を増やす&append_IDsの更新
+                                        do dummyID = self%append_IDs(faceID)%vertexID - 3, self%append_IDs(faceID)%vertexID - 1
+                                            self%re_merge_triangles(faceID)%vertices(dummyID)%coordinate = (/-99,-99,-99/)
+                                            self%re_append_IDs(faceID)%vertexID = self%append_IDs(faceID)%vertexID - 3
+                                        end do
+                                    end if
+                                    !元配列の更新
+                                    do update_faceID = 1, size(self%merge_triangles)
+                                        do update_vertexID = 1, size(self%merge_triangles(faceID)%vertices)
+                                            self%merge_triangles(update_faceID)%vertices(update_vertexID)%coordinate&
+                                            = self%re_merge_triangles(update_faceID)%vertices(update_vertexID)%coordinate
+                                        end do
+                                    end do
+                                    do update_appendID = 1, size(self%append_IDs)
+                                        self%append_IDs(update_appendID)%vertexID = self%re_append_IDs(update_appendID)%vertexID
+                                    end do
+                                end if
+                            end do
+                        end do
+                    end do
+                end if
+            end do
+            do logical_test_ID = 1, size(self%merge_triangles)
+                if(self%append_IDs(logical_test_ID)%vertexID - 1 > 10 .or. &
+                self%append_IDs(logical_test_ID)%vertexID - 1 == 0) then
+                    classification_not_completed(logical_test_ID) = .false.
+                end if
+            end do
+            all_false = all(.not. classification_not_completed)
+        end do
+
+        count = 1
+        do faceID = 1, size(self%merge_triangles)
+            if(self%append_IDs(faceID)%vertexID - 1 > 0)then
+                write(filename,'("triangle_",i0,".stl")') count
+                call output_stl_triangle_group(self, faceID, filename)
+                count = count + 1
+            end if
+        end do
+        print*, "num of face group = ", count - 1
+
+        count = 1
+        open(newunit = n_unit, file = "data/triangle_group_coordinate.csv", status = "replace")
+            do faceID = 1, size(self%merge_triangles)
+                if(self%append_IDs(faceID)%vertexID - 1 > 0)then
+                write(n_unit,'(*(g0:,"  "))') count
+                    do vertexID = 1, self%append_IDs(faceID)%vertexID - 1 !ダミー値を除いて記述
+                        write(n_unit, '(*(g0:," , "))') self%merge_triangles(faceID)%vertices(vertexID)%coordinate
+                    end do
+                    count = count + 1
+                end if
+            end do
+        close(n_unit)
+
+        open(newunit = n_unit, file = "data/triange_group_num.txt", status = "replace")
+            write(n_unit, '(A)') "faceID, num_vertices"
+            do faceID = 1, size(self%merge_triangles)
+                write(n_unit, '(*(g0:," , "))') faceID, self%append_IDs(faceID)%vertexID - 1
+            end do
+        close(n_unit)
+    end subroutine
+
+    subroutine output_csv_triangle_group(self)
+        class(stl_t) self
+        integer n_unit, faceID, vertexID, count
+        character(100) filename
+
+        count = 1
+        do faceID = 1, size(self%merge_triangles)
+            if(self%append_IDs(faceID)%vertexID - 1 > 0)then
+                write(filename, '("triangle_",i0,".csv")') count
+                open(newunit = n_unit, file = "data/triangle/csv/"//trim(filename))
+                    do vertexID = 1, self%append_IDs(faceID)%vertexID - 1
+                        write(n_unit, '(*(g0:," , "))') self%merge_triangles(faceID)%vertices(vertexID)%coordinate
+                    end do
+                    count = count + 1
+                close(n_unit)
+            end if
+        end do
     end subroutine
 
     subroutine sort_dot_with_basic_vector(self)
--- src/main.f90
+++ src/main.f90
@@ -2,20 +2,26 @@ program main
     use stl_operator_m    
     implicit none
 
-    character(256) input_filename, output_filename
+    character(256) input_filename, output_filename, csv_filename
     type(stl_t) stl
 
-    input_filename = "data/airway_inlet_trans.stl"
-    output_filename = "data/test_ascii.stl"
+    input_filename = "data/outlet.stl"
+    output_filename = "data/outlet_ascii.stl"
+    csv_filename = "data/coordinate.csv"
 
     call stl%read_binary_stl(trim(input_filename))
     call stl%output_stl_ascii(trim(output_filename))
-    call stl%output_dot_with_basic_vector()
-    call stl%sort_dot_with_basic_vector()
-    call stl%get_num_of_different_norm_vector()
-    call stl%get_group_of_same_norm_vector()
-    call stl%get_min_max_coordinate_of_group()
-    call stl%output_box_list()
-    call stl%output_box_vtk()
+    call stl%output_coordinate_csv(trim(csv_filename))
+    call stl%make_triangle_groups()
+    call stl%output_csv_triangle_group()
+    ! call stl%output_dot_with_basic_vector()
+    ! call stl%sort_dot_with_basic_vector()
+    ! call stl%get_num_of_different_norm_vector()
+    ! call stl%get_group_of_same_norm_vector()
+
+    !なんかセグメンテーション違反した．とりあえずコメントアウト
+    ! call stl%get_min_max_coordinate_of_group()
+    ! call stl%output_box_list()
+    ! call stl%output_box_vtk()
 
 end program
